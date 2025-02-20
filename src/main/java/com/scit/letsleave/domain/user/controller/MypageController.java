160a38b44b73281b7441b71a1e5da77c3ed1fc6d
KIM SEUNGHYEOK <shkim8745@gmail.com>
2025-02-20 18:01:33 +0900

    controller edited

--- src/main/java/com/scit/letsleave/domain/user/controller/MypageController.java
+++ src/main/java/com/scit/letsleave/domain/user/controller/MypageController.java
@@ -17,13 +17,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.scit.letsleave.domain.user.dto.MypageDTO;
 import com.scit.letsleave.domain.user.entity.MypageEntity;
 import com.scit.letsleave.domain.user.service.MypageService;
 
 import jakarta.servlet.http.HttpSession;
-
 
 @RestController
 @RequiredArgsConstructor
@@ -37,9 +37,9 @@ public class MypageController {
 	public ResponseEntity<MypageEntity> getProfile(HttpSession session) {
         Long id = (Long) session.getAttribute("id");
         if (id == null) {
-//        	id = 1L;
-//        	session.setAttribute("id", id);
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+       	id = 1L;
+       	session.setAttribute("id", id);
+        //return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
         Optional<MypageEntity> userOpt = service.getUserById(id);
         return userOpt.map(ResponseEntity::ok)
@@ -53,7 +53,9 @@ public class MypageController {
 			Long id = (Long) session.getAttribute("id");
 			
 			  if (id == null) {
-			        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+					id = 1L;
+       				session.setAttribute("id", id);
+			        //return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
 			    }
 			    
 			    service.updateProfile(id, dto);

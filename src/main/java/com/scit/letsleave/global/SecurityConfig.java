2294b6b4ebdac716b1bedd07b1a43a46068604b5
YuJung <kimyujung1026@gmail.com>
2025-02-26 15:54:59 +0900

    .

--- src/main/java/com/scit/letsleave/global/SecurityConfig.java
+++ src/main/java/com/scit/letsleave/global/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -39,8 +40,17 @@ public class SecurityConfig {
                         , "/users/auth/**"
                         , "/users/signup"
                         , "/users/login"
-                        , "/script/**").permitAll()
+                        , "/script/**"
+                        , "/css/**"
+                        , "/images/**"
+                        , "/favicon.ico"
+                        , "/guides/**"
+                        , "/schedules/**").permitAll()
                .anyRequest().authenticated()
+           )
+           .formLogin(form -> form
+                .loginPage("/users/login") // 로그인 페이지 경로 설정
+                .permitAll() // 로그인 페이지는 누구나 접근 가능
            )
            .logout(logout -> logout
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) // 로그아웃 URL 설정
@@ -48,6 +58,9 @@ public class SecurityConfig {
                 .invalidateHttpSession(true) // 세션 무효화
                 .clearAuthentication(true) // 인증 정보 제거
                 .deleteCookies("Authorization") // Authorization 쿠키 삭제
+            )
+            .exceptionHandling(exception -> exception
+                .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/users/login")) // 인증되지 않은 사용자 처리
             )
            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 

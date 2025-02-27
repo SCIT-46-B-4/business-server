45fb2e988f27678e608e490e0a261d1d9f9699ee
Seunghyeok Kim <139187561+Seunghyeok8745@users.noreply.github.com>
2025-02-27 19:58:45 +0900

    feat(user): Add my page(#18)
    - Add my page views
    - Add user profile retrieve feat
    - Add user profile update feat

--- src/main/java/com/scit/letsleave/domain/user/service/UserService.java
+++ src/main/java/com/scit/letsleave/domain/user/service/UserService.java
@@ -1,25 +1,53 @@
 package com.scit.letsleave.domain.user.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import com.scit.letsleave.domain.user.dto.UserDto;
 import com.scit.letsleave.domain.user.entity.UserEntity;
 import com.scit.letsleave.domain.user.repository.UserRepository;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
+
 public class UserService {
+    private final UserRepository repository;
+	
+       /**
+        * 주어진 사용자 ID에 해당하는 사용자의 프로필 정보를 업데이트
+        *
+        * @param id   업데이트할 사용자의 고유 식별자 (PK)
+        * @param dto  업데이트할 프로필 정보를 담은 UserDto 객체
+        */
 
-    private final UserRepository userRepository;
+	@Transactional
+	public void updateProfile(Long id, UserDto dto) {
 
+		Optional<UserEntity> userInfo = repository.findById(id);
+		if(userInfo.isPresent()) {
+			UserEntity user = userInfo.get();
+			user.setNickname(dto.getNickname());
+			user.setIsAgreeLoc(dto.getIsAgreeLoc());
+			user.setIsAgreeMarketingNoti(dto.getIsAgreeMarketingNoti());
+			user.setIsAgreeNewsNoti(dto.getIsAgreeNewsNoti());
+			user.setProfileImg(dto.getProfileImg());
+			repository.save(user);
+		}
+	}
+    
     /**
      * 이메일 존재 여부 확인.
      * @param email 이메일 주소.
      * @return 존재하면 true, 그렇지 않으면 false.
      */
     public boolean isEmailExists(String email) {
-        return userRepository.existsByEmail(email); // existsBy로 변경됨
+        return repository.existsByEmail(email); // existsBy로 변경됨
     }
 
     /**
@@ -28,7 +56,7 @@ public class UserService {
      * @return 존재하면 true, 그렇지 않으면 false.
      */
     public boolean isPhoneExists(String phone) {
-        return userRepository.existsByPhone(phone); // existsBy로 변경됨
+        return repository.existsByPhone(phone); // existsBy로 변경됨
     }
 
     /**
@@ -37,7 +65,7 @@ public class UserService {
      * @return UserEntity 사용자 엔티티 또는 null (존재하지 않을 경우)
      */
     public UserEntity findByEmail(String email) {
-        return userRepository.findByEmail(email).orElse(null);
+        return repository.findByEmail(email).orElse(null);
     }
 
     /**
@@ -46,6 +74,6 @@ public class UserService {
      * @return UserEntity 사용자 엔티티 또는 null (존재하지 않을 경우)
      */
     public UserEntity findById(Long id) {
-        return userRepository.findById(id).orElse(null); // Optional 처리
+        return repository.findById(id).orElse(null); // Optional 처리
     }
 }

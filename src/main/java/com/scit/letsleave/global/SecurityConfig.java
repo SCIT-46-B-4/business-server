c4fd5fbba8b39a0565757c2bf68855453437beb8
YuJung <kimyujung1026@gmail.com>
2025-03-06 20:15:56 +0900

    style(member-social): Update Google login button with CSS template

--- src/main/java/com/scit/letsleave/global/SecurityConfig.java
+++ src/main/java/com/scit/letsleave/global/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
            .authorizeHttpRequests(auth -> auth
                .requestMatchers(
                         "/"
-                        , "/**" // 임시
+                        // , "/**" // 임시
                         , "/users/auth/**"
                         , "/users/signup"
                         , "/users/login"

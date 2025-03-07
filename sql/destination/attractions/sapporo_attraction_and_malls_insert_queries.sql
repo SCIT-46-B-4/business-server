9b90d38dc281fd4a4e1dd432897724c38dbc39d2
Bang <windy01999@gmail.com>
2025-03-07 16:17:41 +0900

    feat(db): add tokyo, fukuoka, osaka, sapporo attraction, shopping mall

---
+++ sql/destination/attractions/sapporo_attraction_and_malls_insert_queries.sql
@@ -0,0 +1,181 @@
+INSERT INTO destinations (
+        city_id, type, kr_name, loc_name, title, content, coordinate, address, 
+        contact, homepage, how_to_go, available_time, score, title_img, feature
+    ) 
+    VALUES (
+        4, 1, '삿포로 맥주 박물관', 'サッポロビール博物館', '삿포로 맥주 박물관', '현재는 맥주 박물관으로 운영되는 옛 공장으로 전시회, 투어, 유료 시음이 제공되며, 무료 입장이 가능합니다.', 
+        ST_GeomFromText('POINT(43.0714671 141.3663375)', 4326), '9 Chome-1-1 Kita 7 Johigashi, Higashi Ward, Sapporo, Hokkaido 065-8633 일본 ', NULL, 'http://www.sapporobeer.jp/brewery/s_museum/', '苗穂駅', 
+        '금요일,오전 11:00~오후 6:00, 토요일,오전 11:00~오후 6:00, 일요일,오전 11:00~오후 6:00, 월요일,오전 11:00~오후 6:00, 화요일,오전 11:00~오후 6:00, 수요일,오전 11:00~오후 6:00, 목요일,오전 11:00~오후 6:00', 4.3, '0040c646-23f6-4f87-bb32-15eaee91e0e6.jpg', '{"접근성": ["휠체어 이용 가능 출입구", "휠체어 이용가능 주차장", "휠체어 이용가능 화장실"], "서비스 옵션": ["현장 서비스"], "편의 시설": ["음식점", "화장실"]}'
+    );
+INSERT INTO destinations (
+        city_id, type, kr_name, loc_name, title, content, coordinate, address, 
+        contact, homepage, how_to_go, available_time, score, title_img, feature
+    ) 
+    VALUES (
+        4, 1, '오도리 공원', '大通公園', '오도리 공원', '공원', 
+        ST_GeomFromText('POINT(43.060752 141.3495599)', 4326), '일본 〒060-0042 Hokkaido, Sapporo, Chuo Ward, Odorinishi, １〜１２丁目 ', NULL, 'https://odori-park.jp/', '大通駅', 
+        '금요일,24시간 영업, 토요일,24시간 영업, 일요일,24시간 영업, 월요일,24시간 영업, 화요일,24시간 영업, 수요일,24시간 영업, 목요일,24시간 영업', 4.5, 'd073163a-d976-4c19-ac52-eab97ea20112.jpg', '{"접근성": ["휠체어 이용 가능 출입구"], "서비스 옵션": ["현장 서비스"], "편의 시설": ["공중화장실"], "아동": ["어린이에게 적합함"], "반려동물": ["반려견 동반 가능"]}'
+    );
+INSERT INTO destinations (
+        city_id, type, kr_name, loc_name, title, content, coordinate, address, 
+        contact, homepage, how_to_go, available_time, score, title_img, feature
+    ) 
+    VALUES (
+        4, 1, '삿포로 TV 타워', 'さっぽろテレビ塔', '삿포로 TV 타워', '1950년대에 지어진 유명한 TV 타워로 도시의 탁 트인 전망을 제공하는 전망대가 있습니다.', 
+        ST_GeomFromText('POINT(43.0611047 141.3538497)', 4326), '1 Chome Odorinishi, Chuo Ward, Sapporo, Hokkaido 060-0042 일본 ', NULL, 'https://www.tv-tower.co.jp/', '大通駅', 
+        '금요일,오전 9:00~오후 10:00, 토요일,오전 9:00~오후 10:00, 일요일,오전 9:00~오후 10:00, 월요일,오전 9:00~오후 10:00, 화요일,오전 9:00~오후 10:00, 수요일,오전 9:00~오후 10:00, 목요일,오전 9:00~오후 10:00', 4.1, 'd8e89f80-e097-4402-84f8-e2b63960a77f.jpg', '{"접근성": ["휠체어 이용 가능 출입구"], "서비스 옵션": ["현장 서비스"], "아동": ["어린이에게 적합함"]}'
+    );
+INSERT INTO destinations (
+        city_id, type, kr_name, loc_name, title, content, coordinate, address, 
+        contact, homepage, how_to_go, available_time, score, title_img, feature
+    ) 
+    VALUES (
+        4, 1, '시로이코이비토 파크', '白い恋人パーク', '시로이코이비토 파크', '초콜릿 및 쿠키 샘플과 전시관이 있어 가족 명소로 유명하며 트레인 놀이기구와 정원이 있습니다.', 
+        ST_GeomFromText('POINT(43.0888751 141.2691293)', 4326), '2 Chome-11-36 Miyanosawa 2 Jo, Nishi Ward, Sapporo, Hokkaido 063-0052 일본 ', NULL, 'https://www.shiroikoibitopark.jp/?utm_source=google&utm_medium=maps', '宮の沢駅', 
+        '금요일,오전 10:00~오후 7:00, 토요일,오전 10:00~오후 7:00, 일요일,오전 10:00~오후 7:00, 월요일,오전 10:00~오후 7:00, 화요일,오전 10:00~오후 7:00, 수요일,오전 10:00~오후 7:00, 목요일,오전 10:00~오후 7:00', 4.2, '62406b76-fa71-4f77-bff7-25c1ead92592.jpg', '{"접근성": ["휠체어 이용 가능 출입구", "휠체어 이용가능 주차장", "휠체어 이용가능 화장실"], "서비스 옵션": ["현장 서비스"], "상품/서비스": ["음식"], "편의 시설": ["음식점", "화장실"], "아동": ["어린이에게 적합함"], "주차": ["유료 주차장"]}'
+    );
+INSERT INTO destinations (
+        city_id, type, kr_name, loc_name, title, content, coordinate, address, 
+        contact, homepage, how_to_go, available_time, score, title_img, feature
+    ) 
+    VALUES (
+        4, 1, '홋카이도청', '北海道庁', '홋카이도청', '홋카이도 역사가 전시되어 있는 1880년대식의 붉은 벽돌 건물로, 과거에는 도청이었습니다.', 
+        ST_GeomFromText('POINT(43.0643091 141.3442575)', 4326), '6 Chome Kita 3 Jonishi, Chuo Ward, Sapporo, Hokkaido 060-0003 일본 ', NULL, 'http://www.pref.hokkaido.lg.jp/', '札幌駅', 
+        '금요일,오전 8:45~오후 5:30, 토요일,휴무일, 일요일,휴무일, 월요일,오전 8:45~오후 5:30, 화요일,오전 8:45~오후 5:30, 수요일,오전 8:45~오후 5:30, 목요일,오전 8:45~오후 5:30', 4.1, 'fa236a19-8f4b-49ad-8949-ef45a29de523.jpg', '{"접근성": ["휠체어 이용 가능 출입구", "휠체어 이용가능 주차장"], "서비스 옵션": ["현장 서비스"]}'
+    );
+INSERT INTO destinations (
+        city_id, type, kr_name, loc_name, title, content, coordinate, address, 
+        contact, homepage, how_to_go, available_time, score, title_img, feature
+    ) 
+    VALUES (
+        4, 1, '나카지마 공원', '中島公園', '나카지마 공원', '심포니 홀, 문학 박물관, 보트 대여소, 찻집이 있고 멋진 조경의 정원을 갖춘 도심 공원입니다.', 
+        ST_GeomFromText('POINT(43.0444888 141.3518398)', 4326), '1 Nakajimakoen, Chuo Ward, Sapporo, Hokkaido 064-0931 일본 ', NULL, 'https://www.sapporo-park.or.jp/nakajima/', '中島公園駅', 
+        '금요일,24시간 영업, 토요일,24시간 영업, 일요일,24시간 영업, 월요일,24시간 영업, 화요일,24시간 영업, 수요일,24시간 영업, 목요일,24시간 영업', 4.3, '9a6f94b1-42c8-4227-9c0d-ef1281c6ef85.jpg', '{"접근성": ["휠체어 이용 가능 출입구"], "액티비티": ["하이킹"], "편의 시설": ["공중화장실", "그네", "미끄럼틀"], "아동": ["놀이터", "아동에게 적합한 하이킹 코스", "어린이에게 적합함"], "반려동물": ["반려견 동반 가능"]}'
+    );
+INSERT INTO destinations (
+        city_id, type, kr_name, loc_name, title, content, coordinate, address, 
+        contact, homepage, how_to_go, available_time, score, title_img, feature
+    ) 
+    VALUES (
+        4, 3, '다누키코지 상점가', '狸小路商店街', '다누키코지 상점가', '7개 블록에 걸쳐 약 200개의 아울렛이 늘어선 지붕 덮인 오래된 쇼핑가입니다.', 
+        ST_GeomFromText('POINT(43.0572386 141.3501021)', 4326), '일본 〒060-0062 Hokkaido, Sapporo, Chuo Ward, 南2・3条西1～7丁目 ', '+81 11-241-5125', 'http://www.tanukikoji.or.jp/', 'すすきの駅
+', 
+        NULL, 4.2, 'e0c0a297-6f08-4553-be32-40706ee5b8cd.jpg', '{"접근성": ["휠체어 이용 가능 출입구"], "서비스 옵션": ["현장 서비스"]}'
+    );
+INSERT INTO destinations (
+        city_id, type, kr_name, loc_name, title, content, coordinate, address, 
+        contact, homepage, how_to_go, available_time, score, title_img, feature
+    ) 
+    VALUES (
+        4, 1, '마루야마 공원', '円山公園', '마루야마 공원', '숲이 우거진 아름다운 공원, 15m 높이의 폭포로 유명한 국립 신사, 전망대가 있습니다.', 
+        ST_GeomFromText('POINT(43.0557453 141.3100324)', 4326), '일본 〒064-0959 Hokkaido, Sapporo, Chuo Ward, Miyagaoka, ３ ', NULL, 'http://maruyamapark.jp/', '円山公園駅', 
+        '금요일,24시간 영업, 토요일,24시간 영업, 일요일,24시간 영업, 월요일,24시간 영업, 화요일,24시간 영업, 수요일,24시간 영업, 목요일,24시간 영업', 4.3, '81bc9f64-0736-4ec7-87f5-5d831bb26ae3.jpg', '{"접근성": ["휠체어 이용 가능 출입구", "휠체어 이용가능 주차장"], "서비스 옵션": ["현장 서비스"], "액티비티": ["하이킹"], "편의 시설": ["공중화장실", "그네", "미끄럼틀", "야구장", "피크닉 테이블"], "아동": ["놀이터", "아동에게 적합한 하이킹 코스", "어린이에게 적합함"], "반려동물": ["반려견 동반 가능"]}'
+    );
+INSERT INTO destinations (
+        city_id, type, kr_name, loc_name, title, content, coordinate, address, 
+        contact, homepage, how_to_go, available_time, score, title_img, feature
+    ) 
+    VALUES (
+        4, 1, '모이와야마 전망대', '藻岩山 山頂展望台', '모이와야마 전망대', '전망대', 
+        ST_GeomFromText('POINT(43.022197 141.3198846)', 4326), 'Moiwa Sancho Station, 1 Moiwayama, Minami Ward, Sapporo, Hokkaido 005-0041 일본 ', NULL, 'https://mt-moiwa.jp/', '札幌もいわ山ロープウェイ 山麓駅', 
+        '금요일,오전 10:30~오후 10:00, 토요일,오전 10:30~오후 10:00, 일요일,오전 10:30~오후 10:00, 월요일,오전 10:30~오후 10:00, 화요일,오전 10:30~오후 10:00, 수요일,오전 10:30~오후 10:00, 목요일,오전 10:30~오후 10:00', 4.5, '3c5b97a3-5547-4a05-ade7-761ac8a94c60.jpg', '{"접근성": ["휠체어 이용 가능 출입구", "휠체어 이용가능 주차장"], "서비스 옵션": ["현장 서비스"], "아동": ["어린이에게 적합함"], "주차": ["무료 주차장", "자체 주차장"]}'
+    );
+INSERT INTO destinations (
+        city_id, type, kr_name, loc_name, title, content, coordinate, address, 
+        contact, homepage, how_to_go, available_time, score, title_img, feature
+    ) 
+    VALUES (
+        4, 1, 'JR타워 전망대', 'JRタワー展望室タワー・スリーエイト', 'JR타워 전망대', '도시의 360도 전경을 감상할 수 있고 카페, 선물 가게, 화장실을 갖춘 38층 전망대입니다.', 
+        ST_GeomFromText('POINT(43.0681393 141.3498536)', 4326), '일본 〒060-0005 Hokkaido, Sapporo, Chuo Ward, Kita 5 Jonishi, 2 Chome−5−番地 JRタワ 38F 受付：JRタワーイースト6Ｆ（札幌ステラプレイス イースト6Ｆ ', NULL, 'http://www.jr-tower.com/t38', '札幌駅', 
+        '금요일,오전 10:00~오후 10:00, 토요일,오전 10:00~오후 10:00, 일요일,오전 10:00~오후 10:00, 월요일,오전 10:00~오후 10:00, 화요일,오전 10:00~오후 10:00, 수요일,오전 10:00~오후 10:00, 목요일,오전 10:00~오후 10:00', 4.5, '49d15cb2-2f54-4eac-9947-8383557f7c4a.jpg', '{"접근성": ["휠체어 이용 가능 출입구", "휠체어 이용가능 주차장"], "서비스 옵션": ["현장 서비스"], "아동": ["어린이에게 적합함"], "주차": ["유료 주차 빌딩", "자체 주차장"]}'
+    );
+INSERT INTO destinations (
+        city_id, type, kr_name, loc_name, title, content, coordinate, address, 
+        contact, homepage, how_to_go, available_time, score, title_img, feature
+    ) 
+    VALUES (
+        4, 1, '삿포로시 마루야마 동물원', '札幌市円山動物園', '삿포로시 마루야마 동물원', '북극곰, 레서판다, 펭귄, 파충류를 가까이에서 볼 수 있는 가족 나들이에 적합한 동물원입니다.', 
+        ST_GeomFromText('POINT(43.0515126 141.3052823)', 4326), '3 Chome-1 Miyagaoka, Chuo Ward, Sapporo, Hokkaido 064-0959 일본 ', NULL, 'https://www.city.sapporo.jp/zoo/', '円山公園駅', 
+        '금요일,오전 9:30~오후 4:00, 토요일,오전 9:30~오후 4:00, 일요일,오전 9:30~오후 4:00, 월요일,오전 9:30~오후 4:00, 화요일,오전 9:30~오후 4:00, 수요일,오전 9:30~오후 4:00, 목요일,오전 9:30~오후 4:00', 4.3, '10c6574a-550f-4b58-9a84-941415f2c131.jpg', '{"접근성": ["휠체어 이용 가능 출입구", "휠체어 이용가능 주차장"], "서비스 옵션": ["현장 서비스"], "아동": ["어린이에게 적합함"]}'
+    );
+INSERT INTO destinations (
+        city_id, type, kr_name, loc_name, title, content, coordinate, address, 
+        contact, homepage, how_to_go, available_time, score, title_img, feature
+    ) 
+    VALUES (
+        4, 1, '북해도 개척촌(홋카이도 개척촌)', '野外博物館 北海道開拓の村', '북해도 개척촌(홋카이도 개척촌)', '19세기의 농촌 마을을 재현한 야외 박물관으로 60채가 넘는 전통 목재 및 석조 가옥이 모여 있습니다.', 
+        ST_GeomFromText('POINT(43.0482239 141.4944206)', 4326), 'Konopporo-50-1 Atsubetsucho, Atsubetsu Ward, Sapporo, Hokkaido 004-0006 일본 ', NULL, 'https://www.kaitaku.or.jp/?utm_source=GBP&utm_medium=GBP&utm_term=GBP&utm_content=GBP&utm_campaign=GBP', '森林公園駅', 
+        '금요일 오전 9:00~오후 4:30, 토요일 오전 9:00~오후 4:30, 일요일 오전 9:00~오후 4:30, 월요일 휴무일, 화요일 오전 9:00~오후 4:30, 수요일 오전 9:00~오후 4:30, 목요일 오전 9:00~오후 4:30', 4.3, 'a81d7fae-a1d7-4043-9095-349d1746a1ed.jpg', '{"접근성": ["휠체어 이용 가능 출입구", "휠체어 이용가능 주차장", "휠체어 이용가능 화장실", "휠체어 대여"], "서비스 옵션": ["현장 서비스"], "편의 시설": ["음식점", "투어 제공", "화장실", "Wi-Fi", "무료 Wi-Fi"], "주요 방문자": ["유아동반/가족모임에 적합"], "아동": ["기저귀 교환대 있음", "수유실", "어린이에게 적합함"], "주차": ["무료 주차장", "자체 주차장"]}'
+    );
+INSERT INTO destinations (
+        city_id, type, kr_name, loc_name, title, content, coordinate, address, 
+        contact, homepage, how_to_go, available_time, score, title_img, feature
+    ) 
+    VALUES (
+        4, 1, '삿포로 시 시계탑', '札幌市時計台', '삿포로 시 시계탑', '1878년에 지은 유명한 목조 시계탑에 현재는 삿포로의 역사가 전시된 박물관이 들어서 있습니다.', 
+        ST_GeomFromText('POINT(43.0625768 141.3509179)', 4326), '2 Chome Kita 1 Jonishi, Chuo Ward, Sapporo, Hokkaido 060-0001 일본 ', NULL, 'http://sapporoshi-tokeidai.jp/', '大通駅', 
+        '금요일,오전 8:45~오후 5:10, 토요일,오전 8:45~오후 5:10, 일요일,오전 8:45~오후 5:10, 월요일,오전 8:45~오후 5:10, 화요일,오전 8:45~오후 5:10, 수요일,오전 8:45~오후 5:10, 목요일,오전 8:45~오후 5:10', 3.8, '37d04809-7b42-4322-ac44-1e64cde56fec.jpg', '{"서비스 옵션": ["현장 서비스"], "아동": ["어린이에게 적합함"]}'
+    );
+INSERT INTO destinations (
+        city_id, type, kr_name, loc_name, title, content, coordinate, address, 
+        contact, homepage, how_to_go, available_time, score, title_img, feature
+    ) 
+    VALUES (
+        4, 3, '다이마루 삿포로점', '大丸札幌店', '다이마루 삿포로점', '백화점', 
+        ST_GeomFromText('POINT(43.0675864 141.3467376)', 4326), '4 Chome-7 Kita 5 Jonishi, Chuo Ward, Sapporo, Hokkaido 060-0005 일본 ', '+81 50-1780-6000', 'https://www.daimaru.co.jp/sapporo/?utm_source=google_map&utm_medium=social&utm_campaign=info&utm_content=07', '札幌駅', 
+        '금요일,오전 10:00~오후 8:00, 토요일,오전 10:00~오후 8:00, 일요일,오전 10:00~오후 8:00, 월요일,오전 10:00~오후 8:00, 화요일,오전 10:00~오후 8:00, 수요일,오전 10:00~오후 8:00, 목요일,오전 10:00~오후 8:00', 4.1, '846e5b5f-436f-45eb-b036-cfdbb89716a0.jpg', '{"접근성": ["휠체어 이용 가능 출입구", "휠체어 이용가능 주차장"], "서비스 옵션": ["배달 서비스", "매장 수령", "매장 내 쇼핑", "현장 서비스"], "상품/서비스": ["남성 의류", "베이커리", "여성 의류"], "계획": ["빠른 쇼핑 가능"], "결제": ["신용카드", "au PAY", "dBarai", "IC 교통카드", "PayPay", "Rakuten Pay", "V-Money"], "주차": ["유료 주차 빌딩"]}'
+    );
+INSERT INTO destinations (
+        city_id, type, kr_name, loc_name, title, content, coordinate, address, 
+        contact, homepage, how_to_go, available_time, score, title_img, feature
+    ) 
+    VALUES (
+        4, 3, '삿포로 스텔라 플레이스', '札幌ステラプレイス', '삿포로 스텔라 플레이스', 'JR 타워에 위치한 대형 쇼핑몰로 수백 개의 상점, 푸드코트, 레스토랑, 영화관이 입점해 있습니다.', 
+        ST_GeomFromText('POINT(43.0681096 141.3492289)', 4326), '2 Chome Kita 5 Jonishi, Chuo Ward, Sapporo, Hokkaido 060-0005 일본 ', '+81 11-209-5100', 'http://www.stellarplace.net/', '札幌駅', 
+        '금요일,오전 10:00~오후 9:00, 토요일,오전 10:00~오후 9:00, 일요일,오전 10:00~오후 9:00, 월요일,오전 10:00~오후 9:00, 화요일,오전 10:00~오후 9:00, 수요일,오전 10:00~오후 9:00, 목요일,오전 10:00~오후 9:00', 4.1, '118962cc-46ef-49b9-b0a9-8c6f419ff9df.jpg', '{"접근성": ["휠체어 이용 가능 출입구", "휠체어 이용가능 주차장"], "서비스 옵션": ["현장 서비스"], "결제": ["au PAY", "dBarai", "IC 교통카드", "PayPay", "Rakuten Pay", "V-Money"], "주차": ["유료 주차 빌딩", "유료 주차장"]}'
+    );
+INSERT INTO destinations (
+        city_id, type, kr_name, loc_name, title, content, coordinate, address, 
+        contact, homepage, how_to_go, available_time, score, title_img, feature
+    ) 
+    VALUES (
+        4, 1, '삿포로 히츠지가오카 전망대', 'さっぽろ羊ヶ丘展望台', '삿포로 히츠지가오카 전망대', '목가적인 느낌의 탁 트인 언덕 위 전망을 즐길 수 있으며 윌리엄 S. 클라크 박사 동상과 방목하는 양이 있습니다.', 
+        ST_GeomFromText('POINT(42.9990681 141.3896364)', 4326), '1 Hitsujigaoka, Toyohira Ward, Sapporo, Hokkaido 062-0045 일본 ', NULL, 'http://www.hitsujigaoka.jp/', '福住駅', 
+        '금요일,오전 9:00~오후 5:00, 토요일,오전 9:00~오후 5:00, 일요일,오전 9:00~오후 5:00, 월요일,오전 9:00~오후 5:00, 화요일,오전 9:00~오후 5:00, 수요일,오전 9:00~오후 5:00, 목요일,오전 9:00~오후 5:00', 3.9, '9f1d2522-e285-474e-a4f4-c5d9c6c44262.jpg', '{"접근성": ["휠체어 이용 가능 출입구", "휠체어 이용가능 주차장"], "서비스 옵션": ["현장 서비스"], "아동": ["어린이 할인", "어린이에게 적합함"], "주차": ["무료 주차장", "유료 주차장"]}'
+    );
+INSERT INTO destinations (
+        city_id, type, kr_name, loc_name, title, content, coordinate, address, 
+        contact, homepage, how_to_go, available_time, score, title_img, feature
+    ) 
+    VALUES (
+        4, 3, '삿포로 팩토리', 'サッポロファクトリー', '삿포로 팩토리', '빛으로 가득한 이 세련된 쇼핑몰은 옛 맥주 공장에 자리해 있으며 다양한 쇼핑 매장과 레스토랑이 있습니다.', 
+        ST_GeomFromText('POINT(43.0654276 141.3598746)', 4326), '4 Chome Kita 2 Johigashi, Chuo Ward, Sapporo, Hokkaido 060-0032 일본 ', '+81 11-207-5000', 'https://sapporofactory.jp/', 'バスセンター前駅', 
+        '금요일,오전 10:00~오후 8:00, 토요일,오전 10:00~오후 8:00, 일요일,오전 10:00~오후 8:00, 월요일,오전 10:00~오후 8:00, 화요일,오전 10:00~오후 8:00, 수요일,오전 10:00~오후 8:00, 목요일,오전 10:00~오후 8:00', 3.9, '2bc1e709-1d1b-4fc5-ada7-3976123d5918.jpg', '{"접근성": ["휠체어 이용 가능 출입구", "휠체어 이용가능 주차장"], "서비스 옵션": ["현장 서비스"], "상품/서비스": ["아케이드 게임"], "결제": ["신용카드", "au PAY", "dBarai", "IC 교통카드", "PayPay", "Rakuten Pay", "V-Money"], "주차": ["유료 주차 빌딩", "유료 주차장"]}'
+    );
+INSERT INTO destinations (
+        city_id, type, kr_name, loc_name, title, content, coordinate, address, 
+        contact, homepage, how_to_go, available_time, score, title_img, feature
+    ) 
+    VALUES (
+        4, 1, '삿포로 라멘요코초', '元祖さっぽろラーメン横丁(Ganso Sapporo Ramen Alley)', '삿포로 라멘요코초', '1950년대에 조성된 좁은 골목으로 작은 라면 전문점들이 줄지어 있는 것으로 유명합니다.', 
+        ST_GeomFromText('POINT(43.0547818 141.3518125)', 4326), '일본 〒064-0805 Hokkaido, Sapporo, Chuo Ward, Minami 5 Jonishi, 3 Chome−8番 Ｎ・グランデビル ラーメン横丁 ', NULL, 'http://www.ganso-yokocho.com/', 'すすきの駅', 
+        NULL, 3.9, 'bb4a9fea-b522-4288-b144-8ffa0e269111.jpg', '{"서비스 옵션": ["매장 내 식사"], "인기 있는 이유": ["점심식사", "저녁식사", "1인 식사"], "상품/서비스": ["맥주", "심야 시간대 음식", "주류 제공"], "식사 옵션": ["점심식사", "저녁식사"], "편의 시설": ["화장실"], "분위기": ["캐주얼"], "주요 방문자": ["관광객", "대학생"], "결제": ["신용카드", "체크카드", "NFC 모바일 결제", "신용카드"], "아동": ["어린이에게 적합함"]}'
+    );
+INSERT INTO destinations (
+        city_id, type, kr_name, loc_name, title, content, coordinate, address, 
+        contact, homepage, how_to_go, available_time, score, title_img, feature
+    ) 
+    VALUES (
+        4, 3, '삿포로 파르코', '札幌PARCO', '삿포로 파르코', '쇼핑몰', 
+        ST_GeomFromText('POINT(43.058767 141.3506061)', 4326), '일본 〒060-0061 Hokkaido, Sapporo, Chuo Ward, Minami 1 Jonishi, 3 Chome−３ ', NULL, 'https://sapporo.parco.jp/?utm_source=mybusiness', '大通駅', 
+        '금요일,오전 10:00~오후 8:00, 토요일,오전 10:00~오후 8:00, 일요일,오전 10:00~오후 8:00, 월요일,오전 10:00~오후 8:00, 화요일,오전 10:00~오후 8:00, 수요일,오전 10:00~오후 8:00, 목요일,오전 10:00~오후 8:00', 3.8, '55921458-1952-49fd-a562-472918d62d10.jpg', '{"접근성": ["휠체어 이용 가능 출입구", "휠체어 이용가능 주차장", "휠체어 이용가능 화장실"], "서비스 옵션": ["현장 서비스"], "편의 시설": ["화장실", "무료 Wi-Fi"], "결제": ["신용카드", "체크카드", "au PAY", "dBarai", "IC 교통카드", "PayPay", "Rakuten Pay", "신용카드"], "아동": ["기저귀 교환대 있음", "수유실"], "주차": ["유료 주차장"]}'
+    );
+INSERT INTO destinations (
+        city_id, type, kr_name, loc_name, title, content, coordinate, address, 
+        contact, homepage, how_to_go, available_time, score, title_img, feature
+    ) 
+    VALUES (
+        4, 3, '메가 돈키호테 삿포로 다누키코지 본점', 'MEGAドン・キホーテ 札幌狸小路本店', '메가 돈키호테 삿포로 다누키코지 본점', '할인점', 
+        ST_GeomFromText('POINT(43.0569601 141.3499887)', 4326), '4 Chome-12-1 Minami 3 Jonishi, Chuo Ward, Sapporo, Hokkaido 060-0063 일본 ', '+81 570-096-811', 'https://www.donki.com/store/shop_detail.php?shop_id=467', 'すすきの駅', 
+        '금요일,24시간 영업, 토요일,24시간 영업, 일요일,24시간 영업, 월요일,24시간 영업, 화요일,24시간 영업, 수요일,24시간 영업, 목요일,24시간 영업', 3.7, '622c054a-aa20-4ffa-beff-4297d159f02b.jpg', '{"서비스 옵션": ["매장 밖 수령", "매장 수령", "매장 내 쇼핑", "현장 서비스"], "편의 시설": ["Wi-Fi", "무료 Wi-Fi"], "계획": ["빠른 쇼핑 가능"], "결제": ["신용카드", "au PAY", "dBarai", "IC 교통카드", "PayPay", "Rakuten Pay", "신용카드"]}'
+    );

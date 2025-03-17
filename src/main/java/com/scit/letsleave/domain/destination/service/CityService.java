bb24192d21869ce9e77b58daf04260ebd3917da7
엄예원 <dpdnjs8532@naver.com>
2025-03-17 15:53:42 +0900

    dd

--- src/main/java/com/scit/letsleave/domain/destination/service/CityService.java
+++ src/main/java/com/scit/letsleave/domain/destination/service/CityService.java
@@ -1,13 +1,13 @@
 package com.scit.letsleave.domain.destination.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 import com.scit.letsleave.domain.destination.dto.CityDto;
 import com.scit.letsleave.domain.destination.entity.CityEntity;
 import com.scit.letsleave.domain.destination.repository.CityRepository;
-import com.scit.letsleave.domain.guide.repository.GuidesRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,7 +18,6 @@ import lombok.extern.slf4j.Slf4j;
 public class CityService {
 
     private final CityRepository cityRepository;
-    private final GuidesRepository guidesRepository;
 
     // public List<CityEntity> getPopularCities() {
     // return cityRepository.findTop10ByOrderByIdDesc();
@@ -27,19 +26,20 @@ public class CityService {
     public List<CityDto> getPopularCities(Long countryId) {
         List<CityEntity> entities = cityRepository.findTop10ByOrderByIdDesc(countryId);
         return entities.stream()
-                .map(CityDto::toDTO)
-                .toList();
+                .map(CityDto::toDto)
+                .collect(Collectors.toList());
     }
 
     public List<CityDto> searchCitiesByKrName(String searchWord) {
-        List<CityDto> cities = cityRepository.findByKrNameContaining(searchWord).stream().map(CityDto::toDTO).toList();
+        List<CityDto> cities = cityRepository.findByKrNameContaining(searchWord).stream().map(CityDto::toDto)
+                .collect(Collectors.toList());
         return cities;
     }
 
     // CityService.java (정상 동작 버전)
     public String getCityNameById(Long cityId) {
         return cityRepository.findById(cityId)
-                .map(CityEntity::getKrName) // ✅ CityEntity로 수정
+                .map(CityEntity::getKrName)
                 .orElse("지금");
     }
 

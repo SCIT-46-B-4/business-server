44e21ac45d0034bb9c3e8508855315e00d013c2f
Youn Seojun <92sursumcorda@gmail.com>
2025-03-24 13:36:06 +0900

    guide repo, service modify

--- src/main/java/com/scit/letsleave/domain/guide/repository/GuideRepository.java
+++ src/main/java/com/scit/letsleave/domain/guide/repository/GuideRepository.java
@@ -1,8 +1,11 @@
 package com.scit.letsleave.domain.guide.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.scit.letsleave.domain.guide.entity.GuideEntity;
 
@@ -16,4 +19,11 @@ public interface GuideRepository extends JpaRepository<GuideEntity, Long> {
 
     Page<GuideEntity> findByCityId(Integer cityId, Pageable pageable);
 
+    @Query(value = """
+        SELECT *
+        FROM guides
+        ORDER BY (score * 0.65 + like_count * 0.35) DESC
+        LIMIT 10
+    """, nativeQuery = true)
+    List<GuideEntity> findTop10Guides();
 }

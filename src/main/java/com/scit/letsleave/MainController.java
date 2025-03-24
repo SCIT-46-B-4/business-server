c7364b21e2f8d434665411794085fa8c27d8bf34
Youn Seojun <92sursumcorda@gmail.com>
2025-03-24 13:32:04 +0900

    ddl, main modify

--- src/main/java/com/scit/letsleave/MainController.java
+++ src/main/java/com/scit/letsleave/MainController.java
@@ -1,14 +1,24 @@
 package com.scit.letsleave;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.scit.letsleave.domain.guide.service.GuideService;
+
+import lombok.RequiredArgsConstructor;
 
 
 @Controller
+@RequiredArgsConstructor
 public class MainController {
 
+    private final GuideService guideService;
+
     @GetMapping({"/", ""})
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("guides", guideService.getPopularGuideLimit10());
+
         return "index";
     }
 }
